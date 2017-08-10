8536d0dc01d3874ca17da409abf93bd5eb5708f8
Aviezer Lifshitz <aviezer.lifshitz@weizmann.ac.il>
2017-08-10 17:21:05 +0300

    removed files marked by misha as redundant.
    Former-commit-id: f0cbdcee234b746e132d77a32bcf9c641b325989

--- src/GInterval2D.cpp
+++
@@ -1,8 +0,0 @@
-#include <algorithm>
-
-#include "GInterval2D.h"
-
-//------------------------------------- GInterval2D ---------------------------------------------
-
-const char *GInterval2D::COL_NAMES[GInterval2D::NUM_COLS] = { "chrom1", "start1", "end1", "chrom2", "start2", "end2" };
-
--- src/GIntervalsFetcher2D.cpp
+++
@@ -1,7 +0,0 @@
-#include "GIntervalsFetcher2D.h"
-
-bool GIntervalsFetcher2D::compare_for_sort(const GInterval2D &interv1, const GInterval2D &interv2)
-{
-	return interv1 < interv2;
-}
-

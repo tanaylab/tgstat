866b034712955748b9179beca0fef08fa417228b
Aviezer Lifshitz <18175550+aviezerl@users.noreply.github.com>
2018-04-16 12:57:51 +0300

    tgstat 2.2.1 tgs_knn: return highest knn values instead of highest abs(values)

--- src/matrix_tapply.cpp
+++ src/matrix_tapply.cpp
@@ -1,3 +1,5 @@
+#include <iterator>
+
 #include <R.h>
 #include <Rinternals.h>
 
@@ -5,7 +7,7 @@
 
 extern "C" {
 
-SEXP matrix_tapply(SEXP _x, SEXP _index, SEXP _fun, SEXP _envir)
+SEXP matrix_tapply(SEXP _x, SEXP _index, SEXP _fn, SEXP _envir)
 {
     SEXP answer = R_NilValue;
 //  double *res = (double *)MAP_FAILED;
@@ -22,9 +24,35 @@ SEXP matrix_tapply(SEXP _x, SEXP _index, SEXP _fun, SEXP _envir)
         int num_rows = nrows(_x);
         int num_cols = ncols(_x);
 
-        if (!isInteger(_index) || xlength(_index) <= 1)
+        if (!isInteger(_index) || xlength(_index) < 1)
             verror("\"index\" argument must be a factor");
 
+        if (xlength(_index) != num_cols)
+            verror("Arguments \"x\" and \"index\" must have same length");
+
+        if (!isFunction(_fn) || xlength(_fn))
+            verror("\"fn\" argument must be a function");
+
+        SEXP rcall;
+        SEXP rgroup;
+        int cols[] = { 2, 3, 5, 6, 8 };
+        int group_size = sizeof(cols) / sizeof(cols[0]);
+
+        if (isReal(_x)) {
+            rprotect(rgroup = allocVector(REALSXP, group_size));
+            for (auto col : cols)
+                REAL(rgroup)[0] = REAL(_x)[num_rows * col];
+        } else {
+            rprotect(rgroup = allocVector(INTSXP, group_size));
+            for (auto col : cols)
+                INTEGER(rgroup)[0] = INTEGER(_x)[num_rows * col];
+        }
+
+        rprotect(rcall = lang2(_fn, R_NilValue));
+        SETCADR(rcall, rgroup);
+
+        return eval_in_R(rcall, _envir);
+        
     } catch (TGLException &e) {
 //      if (!TGStat::is_kid() && res != (double *)MAP_FAILED) {
 //          munmap(res, res_sizeof);

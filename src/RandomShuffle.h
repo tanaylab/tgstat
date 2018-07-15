353e72c48a37d146221f252fe7a777c9e15fbbce
Aviezer Lifshitz <18175550+aviezerl@users.noreply.github.com>
2018-07-15 07:50:26 +0300

    tgstat 2.2.6 Bug fix in tgs_graph_cover_resample: internal overflow when the number of nodes is higher than 2^16

--- src/RandomShuffle.h
+++ src/RandomShuffle.h
@@ -4,7 +4,7 @@
 #include <algorithm>
 #include <stdlib.h>
 
-// tgs_random_shuffle replaces std::random_shuffle. It uses drand48 as random number generator.
+// tgs_random_shuffle replaces std::random_shuffle.
 // 
 // std::random_shuffle is implemented differently on Linux and OSX which results in different shuffling.
 // The results achieved on Linux are not reproducible on OSX even if the same seed is used.
--- src/correlation_knn.cpp
+++ src/correlation_knn.cpp
@@ -22,7 +22,7 @@
 
 extern "C" {
 
-SEXP tgs_cor_knn_blas(SEXP _x, SEXP _knn, SEXP _pairwise_complete_obs, SEXP _spearman, SEXP _threshold, SEXP _envir)
+SEXP tgs_cor_knn(SEXP _x, SEXP _knn, SEXP _pairwise_complete_obs, SEXP _spearman, SEXP _threshold, SEXP _envir)
 {
     SEXP answer = R_NilValue;
     char *res = (char *)MAP_FAILED;
@@ -146,6 +146,43 @@ SEXP tgs_cor_knn_blas(SEXP _x, SEXP _knn, SEXP _pairwise_complete_obs, SEXP _spe
             }
         }
 
+//struct Mem {
+//    double *m{NULL};
+//    double *m2{NULL};
+//    double *mask{NULL};
+//    double *n{NULL};
+//    double *sum_x{NULL};
+//    double *sum_y{NULL};
+//    double *sum_xy{NULL};
+//    double *sum_x2{NULL};
+//    double *sum_y2{NULL};
+//    ~Mem() { free(m); free(m2); free(mask); free(n); free(sum_x); free(sum_y); free(sum_xy); free(sum_x2); free(sum_y2); }
+//} mem;
+//
+//int num_rows32 = (int)num_rows;
+//int num_cols32 = (int)num_cols;
+//
+//posix_memalign((void **)&mem.m, 64, sizeof(double) * num_vals);
+//posix_memalign((void **)&mem.m2, 64, sizeof(double) * num_vals);
+//posix_memalign((void **)&mem.mask, 64, sizeof(double) * num_vals);
+//posix_memalign((void **)&mem.n, 64, sizeof(double) * num_cols);
+//posix_memalign((void **)&mem.sum_x, 64, sizeof(double) * num_cols);
+//posix_memalign((void **)&mem.sum_y, 64, sizeof(double) * num_cols);
+//posix_memalign((void **)&mem.sum_xy, 64, sizeof(double) * num_cols);
+//posix_memalign((void **)&mem.sum_x2, 64, sizeof(double) * num_cols);
+//posix_memalign((void **)&mem.sum_y2, 64, sizeof(double) * num_cols);
+//
+//for (size_t i = 0; i < num_vals; ++i) {
+//    if ((isReal(_x) && !R_FINITE(REAL(_x)[i])) || (isInteger(_x) && INTEGER(_x)[i] == NA_INTEGER))
+//        mem.m[i] = mem.m2[i] = mem.mask[i] = 0.;
+//    else {
+//        double val = isReal(_x) ? REAL(_x)[i] : INTEGER(_x)[i];
+//        mem.m[i] = val;
+//        mem.m2[i] = val * val;
+//        mem.mask[i] = 1.;
+//    }
+//}
+
         vdebug("Allocating shared memory for results\n");
 
         // Shared memory structure:
@@ -165,6 +202,7 @@ SEXP tgs_cor_knn_blas(SEXP _x, SEXP _knn, SEXP _pairwise_complete_obs, SEXP _spe
 
         int num_cores = max(1, (int)sysconf(_SC_NPROCESSORS_ONLN));
         int num_processes = (int)min(num_cols, (size_t)num_cores);
+//num_processes=1;
         double num_cols4process = num_cols / (double)num_processes;
 
         ProgressReporter progress;
@@ -182,6 +220,53 @@ SEXP tgs_cor_knn_blas(SEXP _x, SEXP _knn, SEXP _pairwise_complete_obs, SEXP _spe
                 vector<double *> pcors(num_cols);
 
                 for (size_t icol1 = scol; icol1 < ecol; ++icol1) {
+//{
+////    char transa = 'T';
+//char transa = 'T';
+//    double alpha = 1;
+//    double beta = 0;
+//    int inc = 1;
+//
+//    // n = t(mask) x mask[,col]
+//    F77_NAME(dgemv)(&transa, &num_rows32, &num_cols32, &alpha, mem.mask, &num_rows32, mem.mask + (num_rows * icol1), &inc, &beta, mem.n, &inc);
+//
+//    // sum_xy = t(m) x m[,col]
+//    F77_NAME(dgemv)(&transa, &num_rows32, &num_cols32, &alpha, mem.m, &num_rows32, mem.m + (num_rows * icol1), &inc, &beta, mem.sum_xy, &inc);
+//
+//    // sum_x = t(m) x mask[,col]
+//    F77_NAME(dgemv)(&transa, &num_rows32, &num_cols32, &alpha, mem.m, &num_rows32, mem.mask + (num_rows * icol1), &inc, &beta, mem.sum_x, &inc);
+//
+//    // sum_y = t(mask) x m[,col]
+//    F77_NAME(dgemv)(&transa, &num_rows32, &num_cols32, &alpha, mem.mask, &num_rows32, mem.m + (num_rows * icol1), &inc, &beta, mem.sum_y, &inc);
+//
+//    // sum_x2 = t(m^2) x mask[,col]
+//    F77_NAME(dgemv)(&transa, &num_rows32, &num_cols32, &alpha, mem.m2, &num_rows32, mem.mask + (num_rows * icol1), &inc, &beta, mem.sum_x2, &inc);
+//
+//    // sum_y2 = t(mask) x m^2[,col]
+//    F77_NAME(dgemv)(&transa, &num_rows32, &num_cols32, &alpha, mem.mask, &num_rows32, mem.m2 + (num_rows * icol1), &inc, &beta, mem.sum_y2, &inc);
+//}
+//
+//for (size_t i = 0; i < num_cols; ++i) {
+//    double cor = 0;
+//
+//    if (i != icol1) {
+//        double avg_x = mem.sum_x[i] / mem.n[i];
+//        double avg_y = mem.sum_y[i] / mem.n[i];
+//        double cov_xy = mem.sum_xy[i] / mem.n[i] - avg_x * avg_y;
+//        double stddev_x = sqrt(mem.sum_x2[i] / mem.n[i] - avg_x * avg_x);
+//        double stddev_y = sqrt(mem.sum_y2[i] / mem.n[i] - avg_y * avg_y);
+//        cor = cov_xy / (stddev_x * stddev_y);
+//    }
+//
+//    if (!cor || fabs(cor) < threshold)
+//        cor = -2;
+//
+//    cors[i] = cor;
+//    pcors[i] = &cors[i];
+//    ++itr_idx;
+//    TGStat::itr_idx(itr_idx);
+//}
+
                     for (size_t icol2 = 0; icol2 < num_cols; ++icol2) {
                         double cor = 0;
 
@@ -387,310 +472,3 @@ SEXP tgs_cor_knn_blas(SEXP _x, SEXP _knn, SEXP _pairwise_complete_obs, SEXP _spe
 
 }
 
-#ifdef AAAA
-
-
-
-SEXP tgs_cor_knn_blas(SEXP _x, SEXP _knn, SEXP _pairwise_complete_obs, SEXP _spearman, SEXP _threshold, SEXP _envir)
-{
-    SEXP answer = R_NilValue;
-
-    try {
-        struct Mem {
-            double *m{NULL};
-            double *m2{NULL};
-            double *mask{NULL};
-            ~Mem() { free(m); free(m2); free(mask); }
-        } mem;
-
-        TGStat tgstat(_envir);
-
-        if ((!isReal(_x) && !isInteger(_x)) || xlength(_x) < 1)
-            verror("\"x\" argument must be a matrix of numeric values");
-
-        if (!isLogical(_pairwise_complete_obs) || xlength(_pairwise_complete_obs) != 1)
-            verror("\"pairwise.complete.obs\" argument must be a logical value");
-
-        if (!isLogical(_spearman) || xlength(_spearman) != 1)
-            verror("\"spearman\" argument must be a logical value");
-
-        if ((!isReal(_threshold) && !isInteger(_threshold)) || xlength(_threshold) != 1)
-            verror("\"threshold\" argument must be a numeric value");
-
-        SEXP rdim = getAttrib(_x, R_DimSymbol);
-
-        if (!isInteger(rdim) || xlength(rdim) != 2)
-            verror("\"x\" argument must be a matrix of numeric values");
-
-        bool pairwise_complete_obs = asLogical(_pairwise_complete_obs);
-        bool spearman = asLogical(_spearman);
-        double threshold = fabs(asReal(_threshold));
-        size_t num_rows = nrows(_x);
-        size_t num_cols = ncols(_x);
-        int num_rows32 = (int)num_rows;
-        int num_cols32 = (int)num_cols;
-        size_t num_vals = num_rows * num_cols;
-vector<double> vals;
-bool nan_in_vals = false;
-vector<bool> nan_in_col(num_cols, false);
-vector<double> col_vals1(num_rows);
-vector<double> col_vals2(num_rows);
-double *pcol_vals1 = &col_vals1.front();
-double *pcol_vals2 = &col_vals2.front();
-vector<double> *col_vals[2] = { &col_vals1, &col_vals2 };
-
-vals.reserve(num_vals);
-
-for (size_t i = 0; i < num_vals; ++i) {
-    if ((isReal(_x) && !R_FINITE(REAL(_x)[i])) || (isInteger(_x) && INTEGER(_x)[i] == NA_INTEGER)) {
-        nan_in_col[i / num_rows] = true;
-        nan_in_vals = true;
-        vals.push_back(numeric_limits<double>::quiet_NaN());
-    } else
-        vals.push_back(isReal(_x) ? REAL(_x)[i] : INTEGER(_x)[i]);
-}
-
-        if (num_rows <= 1 || num_cols <= 1)
-            verror("\"x\" argument must be a matrix of numeric values");
-
-        vdebug("START BLAS COR\n");
-        // some BLAS implementations ask to align double arrays to 64 for improved efficiency
-        posix_memalign((void **)&mem.m, 64, sizeof(double) * num_vals);
-        posix_memalign((void **)&mem.m2, 64, sizeof(double) * num_vals);
-        posix_memalign((void **)&mem.mask, 64, sizeof(double) * num_vals);
-
-        for (size_t i = 0; i < num_vals; ++i) {
-            if ((isReal(_x) && !R_FINITE(REAL(_x)[i])) || (isInteger(_x) && INTEGER(_x)[i] == NA_INTEGER)) {
-                mem.m[i] = mem.m2[i] = mem.mask[i] = 0.;
-//                nan_in_vals = true;
-//                nan_in_point[i / num_rows] = true;
-            } else {
-                double val = isReal(_x) ? REAL(_x)[i] : INTEGER(_x)[i];
-                mem.m[i] = val;
-                mem.m2[i] = val * val;
-                mem.mask[i] = 1.;
-            }
-        }
-
-        if (spearman && pairwise_complete_obs && nan_in_vals)
-            verror("BLAS implementation of tgs_cor does not support spearman with pairwise.complete.obs when x contains NA / NaN / Inf");
-
-        // replace values with ranks if spearman=T
-//      if (spearman) {
-//          vector<double *> pvals;
-//          pvals.reserve(num_vals);
-//          for (size_t i = 0; i < num_vals; ++i)
-//              pvals.push_back(&mem.m[i]);
-//
-//          for (size_t ipoint = 0; ipoint < num_cols; ++ipoint) {
-//              if (nan_in_point[ipoint])
-//                  continue;
-//
-//              vector<double *>::iterator sival = pvals.begin() + ipoint * num_rows;
-//              vector<double *>::iterator eival = sival + num_rows;
-//              vector<double *>::iterator last_ival = sival;
-//
-//              sort(sival, eival, [](double *p1, double *p2) { return *p1 < *p2; });
-//              for (auto ival = sival; ; ++ival) {
-//                  if (ival == eival || **ival != **last_ival) {
-//                      double rank = ((ival - sival) + (last_ival - sival) - 1) / 2. + 1;
-//
-//                      while (last_ival != ival) {
-//                          **last_ival = rank;
-//                          mem.m2[*last_ival - mem.m] = rank * rank;
-//                          ++last_ival;
-//                      }
-//
-//                      if (ival == eival)
-//                          break;
-//                  }
-//              }
-//          }
-//      }
-
-        int num_cores = max(1, (int)sysconf(_SC_NPROCESSORS_ONLN));
-        int num_processes = (int)min(num_cols, (size_t)num_cores);
-        double num_cols4process = num_cols / (double)num_processes;
-
-        ProgressReporter progress;
-        progress.init(num_cols * num_cols - num_cols, 1);
-
-        vdebug("Num cores: %d, num_processes: %d\n", num_cores, num_processes);
-        TGStat::prepare4multitasking();
-
-        for (int iprocess = 0; iprocess < num_processes; ++iprocess) {
-            if (!TGStat::launch_process()) {     // child process
-                size_t scol = { (size_t)(iprocess * num_cols4process) };
-                size_t ecol = { (size_t)((iprocess + 1) * num_cols4process) };
-                size_t itr_idx = 0;
-
-                for (size_t icol1 = 0; icol1 < num_cols; ++icol1) {
-                    for (size_t icol2 = scol; icol2 < ecol; ++icol2) {
-                        if (icol1 == icol2)
-                            continue;
-
-                        size_t idx1 = icol1 * num_rows;
-                        size_t idx2 = icol2 * num_rows;
-if (spearman) {
-} else {
-    pcol_vals1 = &vals.front() + idx1;
-    pcol_vals2 = &vals.front() + idx2;
-}
-
-                        double sum1 = 0;
-                        double sum2 = 0;
-                        double sum_square1 = 0;
-                        double sum_square2 = 0;
-                        double mean1, mean2, var1, var2;
-                        double sum_xy = 0;
-                        size_t num_finite_pairs = 0;
-
-                        for (size_t i = 0; i < num_rows; ++i) {
-//double val1 = pcol_vals1[i];
-//double val2 = pcol_vals2[i];
-
-//if (!std::isnan(pcol_vals1[i]) && !std::isnan(pcol_vals2[i])) {
-                            if (mem.mask[idx1] && mem.mask[idx2]) {
-sum1 += pcol_vals1[i];
-sum2 += pcol_vals2[i];
-sum_square1 += pcol_vals1[i] * pcol_vals1[i];
-sum_square2 += pcol_vals2[i] * pcol_vals2[i];
-sum_xy += pcol_vals1[i] * pcol_vals2[i];
-//                              sum1 += mem.m[idx1];
-//                              sum2 += mem.m[idx2];
-//                              sum_square1 += mem.m2[idx1];
-//                              sum_square2 += mem.m2[idx2];
-//                              sum_xy += mem.m[idx1] * mem.m[idx2];  // => sum(X*Y)
-                                ++num_finite_pairs;
-                                ++idx1;
-                                ++idx2;
-                            }
-                        }
-
-                        if (num_finite_pairs) {
-                            mean1 = sum1 / num_finite_pairs;
-                            mean2 = sum2 / num_finite_pairs;
-                            var1 = sum_square1 / num_finite_pairs - mean1 * mean1;
-                            var2 = sum_square2 / num_finite_pairs - mean2 * mean2;
-
-                            // calculate correlation
-                            double cor = (sum_xy / num_finite_pairs - mean1 * mean2) / sqrt(var1 * var2);
-                        }
-
-                        ++itr_idx;
-                        TGStat::itr_idx(itr_idx);
-                    }
-                }
-                rexit();
-            }
-        }
-
-        while (TGStat::wait_for_kids(3000))
-            progress.report(TGStat::itr_idx_sum() - progress.get_elapsed_steps());
-
-        progress.report_last();
-//      {
-//              size_t icol1 = 1;
-//              size_t icol2 = 2;
-//              double sum1 = 0;
-//              double sum2 = 0;
-//              double sum_square1 = 0;
-//              double sum_square2 = 0;
-//              double mean1, mean2, stddev1, stddev2;
-//              double sum_xy = 0;
-//              size_t num_finite_pairs = 0;
-//
-//              size_t idx1 = num_rows * icol1;
-//              size_t idx2 = num_rows * icol2;
-//              for (size_t i = 0; i < num_rows; ++i) {
-//                  if (mem.mask[idx1] && mem.mask[idx2]) {
-//                      sum1 += mem.m[idx1];
-//                      sum2 += mem.m[idx2];
-//                      sum_square1 += mem.m2[idx1];
-//                      sum_square2 += mem.m2[idx2];
-//                      sum_xy += mem.m[idx1] * mem.m[idx2];  // => sum(X*Y)
-//                      ++num_finite_pairs;
-//                  }
-//                  ++idx1;
-//                  ++idx2;
-//              }
-//
-//              if (num_finite_pairs) {
-//                  mean1 = sum1 / num_finite_pairs;
-//                  mean2 = sum2 / num_finite_pairs;
-//                  stddev1 = sqrt(sum_square1 / num_finite_pairs - mean1 * mean1);
-//                  stddev2 = sqrt(sum_square2 / num_finite_pairs - mean2 * mean2);
-//
-//                  // calculate correlation
-//                  double cor = (sum_xy / num_finite_pairs - mean1 * mean2) / (stddev1 * stddev2);
-//                  printf("COR1 = %g\n", cor);
-//
-//                  cor = (sum_xy - sum1 * mean2) / //(stddev1 * stddev2 * num_finite_pairs);
-//                      (sqrt((sum_square1 - sum1 * mean1) * (sum_square2 - sum2 * mean2)));
-//                  printf("COR2 = %g\n", cor);
-//              }
-//      }
-
-//
-//int inc = 1;
-//int xcol=2;
-//int ycol=3;
-//printf("X: ");
-//for (int i = 0; i < num_rows32; ++i)
-//printf("%g ", mem.m[num_rows32 * xcol + i]);
-//printf("\n");
-//printf("Y: ");
-//for (int i = 0; i < num_rows32; ++i)
-//printf("%g ", mem.m[num_rows32 * ycol + i]);
-//printf("\n");
-//
-//printf("MASK X: ");
-//for (int i = 0; i < num_rows32; ++i)
-//printf("%g ", mem.mask[num_rows32 * xcol + i]);
-//printf("\n");
-//printf("MASK Y: ");
-//for (int i = 0; i < num_rows32; ++i)
-//printf("%g ", mem.mask[num_rows32 * ycol + i]);
-//printf("\n");
-//
-//double N = F77_NAME(ddot)(&num_rows32, mem.mask + num_rows32 * xcol, &inc, mem.mask + num_rows32 * ycol, &inc);
-//printf("N=%g\n", N);
-//
-//double sum_xy = F77_NAME(ddot)(&num_rows32, mem.m + num_rows32 * xcol, &inc, mem.m + num_rows32 * ycol, &inc);
-//printf("SUM XY = %g\n", sum_xy);
-//
-//double sum_x = F77_NAME(ddot)(&num_rows32, mem.m + num_rows32 * xcol, &inc, mem.mask + num_rows32 * ycol, &inc);
-//printf("SUM X = %g\n", sum_x);
-//
-//double avg_x = sum_x / N;
-//
-//double sum_y = F77_NAME(ddot)(&num_rows32, mem.mask + num_rows32 * xcol, &inc, mem.m + num_rows32 * ycol, &inc);
-//printf("SUM Y = %g\n", sum_y);
-//
-//double avg_y = sum_y / N;
-//
-//double cov_xy = sum_xy / N - avg_x * avg_y;
-//printf("COV = %g (%g / %g - %g * %g)\n", cov_xy, sum_xy, N, avg_x, avg_y);
-//
-//double sum_x2 = F77_NAME(ddot)(&num_rows32, mem.m2 + num_rows32 * xcol, &inc, mem.mask + num_rows32 * ycol, &inc);
-//printf("SUM X^2 = %g\n", sum_x2);
-//
-//double sum_y2 = F77_NAME(ddot)(&num_rows32, mem.mask + num_rows32 * xcol, &inc, mem.m2 + num_rows32 * ycol, &inc);
-//printf("SUM Y^2 = %g\n", sum_y2);
-//
-//double stddev_x = sqrt(sum_x2 / N - avg_x * avg_x);
-//double stddev_y = sqrt(sum_y2 / N - avg_y * avg_y);
-//printf("STDDEV X = %g\n", stddev_x);
-//printf("STDDEV Y = %g\n", stddev_y);
-//
-//double cor_xy = cov_xy / (stddev_x * stddev_y);
-//printf("COR = %g\n", cor_xy);
-    } catch (TGLException &e) {
-        rerror("%s", e.msg());
-    }
-
-    rreturn(answer);
-}
-
-}
-#endif
